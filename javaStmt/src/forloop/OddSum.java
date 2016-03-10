8921e0c59044acf9be809f8ac3638860bd7abe55
inhyeon <yuin4004@naver.com>
2016-03-10 11:12:50 +0900

    java statment 프로젝트 시작 !!

---
+++ javaStmt/src/forloop/OddSum.java
@@ -0,0 +1,32 @@
+package forloop;
+
+
+/**
+ *@file : OddSum.java 
+ *@author inhyeon
+ *@date 2016. 3. 10.
+ *@story
+ */
+public class OddSum {
+	public static void main(String[] args){
+			
+		int sum=0, i=0;
+		System.out.println("1번째방법");
+		for (i = 0; i <10; i+=2) {
+			
+			sum += i;
+			
+		}
+		System.out.println(sum);
+		System.out.println("2번째방법");
+		for (i = 0; i <10; i++) {
+			
+			if(i % 2 ==1)
+			sum += i;
+			
+		}
+		System.out.println(sum);
+		
+	}
+
+}
